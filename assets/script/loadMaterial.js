03bc2e037eae15b25ff67349a940e5f5e5cbced0
satriaer77 <ppicbimasatria@gmail.com>
2022-06-11 18:47:59 +0700

    Menambahkan file font woff2 (Montserrat & M PLUS 1 Code), memperbaiki image width baca materi, menambahkan @font-face di base.css, menambahkan content di halaman materi dan mata-kuliah

--- assets/script/loadMaterial.js
+++ assets/script/loadMaterial.js
@@ -1,24 +1,25 @@
-const matkul      = paramsUrl.get("mata-kuliah");
-const materi      = paramsUrl.get("materi");
+const matkul = paramsUrl.get("mata-kuliah");
+const materi = paramsUrl.get("materi");
 
 
-const authorName      = document.getElementById("author-name");
-const authorPhoto     = document.getElementById("author-photo");
+const authorName = document.getElementById("author-name");
+const authorPhoto = document.getElementById("author-photo");
 const materialContent = document.getElementById("material-content");
-const materialTitle   = document.getElementById("material-title");
+const materialTitle = document.getElementById("material-title");
 const materialSubject = document.getElementById("material-subject");
-const materialCover   = document.getElementById("material-cover-image");
+const materialCover = document.getElementById("material-cover-image");
 
 
-const jsonObject     = JSON.parse(dataMateri);
+const jsonObject = JSON.parse(dataMateri);
 const materialObject = jsonObject["mata-kuliah"];
-const authorObject   = jsonObject["profil-penulis"];
+const authorObject = jsonObject["profil-penulis"];
 
 console.log(authorObject);
 
-authorName.innerHTML      = authorObject[materialObject[matkul]["materi"][materi]["penulis"]]["nama"];
-authorPhoto.src           = "../assets/images/"+authorObject[materialObject[matkul]["materi"][materi]["penulis"]]["foto"];
-materialCover.style.background = "url('../assets/images/materi/"+materialObject[matkul]["materi"][materi]["cover"]+"') no-repeat center top";
+authorName.innerHTML = authorObject[materialObject[matkul]["materi"][materi]["penulis"]]["nama"];
+authorPhoto.src = "../assets/images/" + authorObject[materialObject[matkul]["materi"][materi]["penulis"]]["foto"];
+materialCover.style.background = "url('../assets/images/materi/" + materialObject[matkul]["materi"][materi]["cover"] + "') top center";
+materialCover.style.background.size = "cover";
 materialContent.innerHTML = materialObject[matkul]["materi"][materi]["isi-materi"];
-materialTitle.innerHTML   = materialObject[matkul]["materi"][materi]["judul"];
+materialTitle.innerHTML = materialObject[matkul]["materi"][materi]["judul"];
 materialSubject.innerHTML = materialObject[matkul]["materi"][materi]["judul-mata-kuliah"];
--- assets/script/materi.js
+++ assets/script/materi.js
@@ -1,3 +1,10 @@
+// =------- Tag for make content -------= //
+// <span class='content-sub-heading'></span> ->> Digunakan untuk membuat sub-heading
+// <span class='content-code'></span>  ->> Digunakan untuk membuat content yang berisi code
+// <div class='content-list-element'>ini adalah daftar list<ul><li></li></ul></div> ->> Digunakan untuk membuat content yang berisi list
+// <p class='content-paragraph'></p> ->> Digunakan untuk membuat content yang berisi paragraf
+// <img class='content-image' src='../assets/images/materi/nama_file.jpg'> ->> Digunakan untuk membuat content yang berisi image
+
 dataMateri = `
 {
     "profil-penulis": {
@@ -22,17 +29,17 @@ dataMateri = `
             "judul-mata-kuliah": "Struktur Data",
             "judul": "Struktur Data Stack",
             "cover": "struktur-data_struktur-data-stack_cover.jpg",
-            "isi-materi": "<span class='content-sub-heading'>    Algoritma Struktur Data  </span><p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFO</p><img class='content-image' src='../assets/images/materi/struktur-data_struktur-data-stack_lifo.jpg'>"
+            "isi-materi": "<span class='content-sub-heading'>    Algoritma Struktur Data  </span><span class='content-code'>int day = 4;<br>switch (day) {<br>&ensp;case 1:<br>&ensp;&ensp;System.out.println('Monday');<br>&ensp;&ensp;break;<br>&ensp;case 2:<br>&ensp;&ensp;System.out.println('Tuesday');</span> <div class='content-list-element'>ini adalah daftar list<ul><li>abjad</li><li>jeaasd</li><li>asdsadsad</li></ul></div>  <p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFO</p><img class='content-image' src='../assets/images/materi/struktur-data_struktur-data-stack_lifo.jpg'>"
           },
           "struktur-data-queue": {
             "jumlah-baca": 0,
-            "urutan" : 2,
-            "tanggal":"04 Juni 2022",
+            "urutan" : 3,
+            "tanggal":"10 Juni 2022",
             "penulis": "raihan",
             "judul-mata-kuliah": "Struktur Data",
-            "judul": "Struktur Data Queue",
+            "judul": "Algoritma Bubble Sort",
             "cover": "struktur-data_struktur-data-queue_cover.jpg",
-            "isi-materi": "<span class='content-sub-heading'>    Algoritma Struktur Data  </span><p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFOLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum faucibus tincidunt. Curabitur congue turpis sed tristique varius. Suspendisse suscipit at tellus sed venenatis. Ut nulla enim, consectetur vitae libero condimentum, facilisis vestibulum nibh. Proin nec eros non quam aliquam blandit. Fusce placerat ullamcorper erat, id consequat ligula tristique ut. In tincidunt urna quis rhoncus tristique. Cras neque magna, tincidunt id nunc id, pulvinar sodales purus. Fusce id bibendum eros, eget pellentesque dui. Cras mattis quam nisl, quis porttitor magna lobortis vel. Quisque dapibus dolor eget suscipit ornare. Sed nec auctor nunc.</p><img class='content-image' src='../assets/images/materi/struktur-data_struktur-data-stack_lifo.jpg'><p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFOLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum faucibus tincidunt. Curabitur congue turpis sed tristique varius. Suspendisse suscipit at tellus sed venenatis. Ut nulla enim, consectetur vitae libero condimentum, facilisis vestibulum nibh. Proin nec eros non quam aliquam blandit. Fusce placerat ullamcorper erat, id consequat ligula tristique ut. In tincidunt urna quis rhoncus tristique. Cras neque magna, tincidunt id nunc id, pulvinar sodales purus. Fusce id bibendum eros, eget pellentesque dui. Cras mattis quam nisl, quis porttitor magna lobortis vel. Quisque dapibus dolor eget suscipit ornare. Sed nec auctor nunc.</p>"
+            "isi-materi": "<span class='content-sub-heading'>    Algoritma Bubble Sort  </span><p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFOLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum faucibus tincidunt. Curabitur congue turpis sed tristique varius. Suspendisse suscipit at tellus sed venenatis. Ut nulla enim, consectetur vitae libero condimentum, facilisis vestibulum nibh. Proin nec eros non quam aliquam blandit. Fusce placerat ullamcorper erat, id consequat ligula tristique ut. In tincidunt urna quis rhoncus tristique. Cras neque magna, tincidunt id nunc id, pulvinar sodales purus. Fusce id bibendum eros, eget pellentesque dui. Cras mattis quam nisl, quis porttitor magna lobortis vel. Quisque dapibus dolor eget suscipit ornare. Sed nec auctor nunc.</p><img class='content-image' src='../assets/images/materi/struktur-data_struktur-data-stack_lifo.jpg'><p class='content-paragraph'>    Struktur data stack merupakan struktur data yang menggunakan algoritma LIFOLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum faucibus tincidunt. Curabitur congue turpis sed tristique varius. Suspendisse suscipit at tellus sed venenatis. Ut nulla enim, consectetur vitae libero condimentum, facilisis vestibulum nibh. Proin nec eros non quam aliquam blandit. Fusce placerat ullamcorper erat, id consequat ligula tristique ut. In tincidunt urna quis rhoncus tristique. Cras neque magna, tincidunt id nunc id, pulvinar sodales purus. Fusce id bibendum eros, eget pellentesque dui. Cras mattis quam nisl, quis porttitor magna lobortis vel. Quisque dapibus dolor eget suscipit ornare. Sed nec auctor nunc.</p> <span class='content-code'>int day = 4;<br>switch (day) {<br>&ensp;case 1:<br>&ensp;&ensp;System.out.println('Monday');<br>&ensp;&ensp;break;<br>&ensp;case 2:<br>&ensp;&ensp;System.out.println('Tuesday');</span><img class='content-image' src='../assets/images/materi/struktur-data_struktur-data-queue_cover.jpg'>"
           }
         }
       }
